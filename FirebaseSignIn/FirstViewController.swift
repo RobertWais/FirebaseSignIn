cf9eeded7b54e1b76bc22326d8304c99881793e6
RobertWais <wais.robert@uwlax.edu>
2018-02-27 23:04:36 -0600

    Initial Commit

---
+++ FirebaseSignIn/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FirebaseSignIn
+//
+//  Created by Robert Wais on 2/27/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
